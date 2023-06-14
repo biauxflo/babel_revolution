7433236dcdfb6769c53a560746bb0ffe4ed65b46
Bouchardon <55424950+Bouchardon@users.noreply.github.com>
2023-06-14 09:40:01 +0200

    Update intro_text.js
    Modif de texte

--- public/texts/intro_text.js
+++ public/texts/intro_text.js
@@ -1,11 +1,11 @@
 var intro_text = {
     "intro_title1" : "Prologue",
 
-    "intro_page1" : "<p>« R-Pollëynisez la vi(ll)e ! » Malgré la censure, ce mystérieux tag fleurit sur les murs de la ville \
-        comme sur ceux des réseaux sociaux, à chaque nouveau décret du Centre de la Monoculture, le CMC, toute puissante \
-        institution agro-culturelle régissant à la fois l’agroforesterie et la vie intellectuelle. À quoi exhorte-il exactement ? \
-        Nul ne le sait exactement. Certains affirment que ce sont simplement des poètes qui veulent replanter des roses dans le bitume  \
-        d’autres que ce sont des nostalgiques de la polyculture archaïque ; d’autres enfin que ce sont des insurgés fomentant un coup d’État… \
+    "intro_page1" : "<p>« R-Pollëynisez la vi(ll)e ! » Malgré la censure, ce mystérieux tag fleurit sur les murs de la ville comme \
+        sur ceux des réseaux sociaux, à chaque nouveau décret du Centre de la Monoculture, le CMC, toute puissante institution \
+        agro-culturelle régissant à la fois l’agroforesterie et la vie intellectuelle. À quoi exhorte-il exactement ? Nul ne le sait \
+        précisément. Certains affirment que ce sont simplement des poètes qui veulent replanter des roses dans le bitume d’autres que \
+        ce sont des nostalgiques de la polyculture archaïque ; d’autres enfin que ce sont des insurgés fomentant un coup d’État… \
         Le tag se multiplie à l’annonce prochaine d’un nouveau décret qui supprimerait des centaines de noms de fleurs, de plantes, d’arbres \
         du Dictionnaire officiel du CMC, dernier d’une longue série de lois et amendements visant à expurger la Langue commune pour \
         « l’actualiser et la mettre mieux en adéquation avec l’environnement », comme le stipule le Dictionnaire officiel du CMC. \
@@ -15,19 +15,19 @@ var intro_text = {
 
     "intro_page2" : "<p>Sous ce tag, des posts publiés dans la DarkMachine rendent témoignage de l’appauvrissement et de la colonisation de la nature, \
         des cultures et du savoir par l’homme, mais aussi de la résistance qui s’organise contre cet étiolement conjoint de la nature et de l'esprit. \
-        Assignées derrière leurs écrans, les dissidents résistent à coups de posts, dont le plurilinguisme même est déjà résistance, au cœur même codé de la Machine. \
-        Ainsi, spontanément, des voix y germent, d’abord balbutiantes et isolées, bientôt articulées, en des langues éradiquées au Coup d’État : naguères florissantes, \
-        comme l’anglais, le mandarin, l’hindi, l’espagnol, l’arabe, le bengali, le russe ; mais aussi de cultures désormais bien lointaines, comme l’étrusque, \
-        le trégorois ou les créoles…
+        Assignées derrière leurs écrans, les dissidents résistent à coups de posts, dont le plurilinguisme même est déjà résistance, au cœur même codé \
+        de la Machine. Ainsi, spontanément, des voix y germent, d’abord balbutiantes et isolées, bientôt articulées, en des langues éradiquées au \
+        Coup d’État : naguères florissantes, comme l’anglais, le mandarin, l’hindi, l’espagnol, l’arabe, le bengali, le russe ; mais aussi de cultures \
+        désormais bien lointaines, comme l’étrusque, le trégorois ou les créoles…
 </p>",
     
     "intro_title3" : "Prologue",
 
-    "intro_page3" : "<p>Suite à l’actualisation du dictionnaire et en dépit de l’interdiction de parler ces langues interdites, le mur de la Machine \
-    se couvre de graphes et de glyphes numériques ; de technotextes, de typoèmes et d’éclats de voix hypermédiatisés ; de fenêtres ouvertes sur un ailleurs \
-    hypertextuel plurilingue devenu zone prohibée ; de souvenirs de rencontres hétérolingues hétérodoxes ; d’exhortations à cultiver son jardin, là, maintenant, \
-    même dans la plus infime fissure de la langue et du bitume. Et qu’importe si les langues se défont ou se fondent, se répondent, s’intriquent à s’en empêtrer \
-    les consonnes... Et qui sait si cette polyphonie en toutes langues n’ouvrait pas un chemin vers une diversité revivifiée : en langue mais aussi en germe ? \
-    Et si ce n’était pas trop tard ? S’il y avait une manière de revenir en arrière ? Et de faire refleurir le monde dans des cultures diverses ? Oui, mais comment ? \
-    Le savez-vous, vous ?</p>"
+    "intro_page3" : "<p>Suite à l’actualisation du dictionnaire et en dépit de l’interdiction de parler ces langues interdites, le mur de la \
+    Machine se couvre de graphes et de glyphes numériques ; de technotextes, de typoèmes et d’éclats de voix hypermédiatisés ; de fenêtres \
+    ouvertes sur un ailleurs hypertextuel plurilingue devenu zone prohibée ; de souvenirs de rencontres hétérolingues hétérodoxes ; d’exhortations \
+    à cultiver son jardin, là, maintenant, même dans la plus infime fissure de la langue et du bitume. Et qu’importe si les langues se défont ou \
+    se fondent, se répondent, s’intriquent à s’en empêtrer les consonnes... Et qui sait si cette polyphonie en toutes langues n’ouvrait pas un chemin \
+    vers une diversité revivifiée : en langue mais aussi en germe ? Et si ce n’était pas trop tard ? S’il y avait une manière de revenir en arrière ? \
+    Et de faire refleurir le monde dans des cultures diverses ? Oui, mais comment ? Le savez-vous, vous ?</p>"
 }
