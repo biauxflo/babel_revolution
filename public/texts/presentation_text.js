f4b3241620fa6dd7e41a10456745f82608bec4fe
Bouchardon <55424950+Bouchardon@users.noreply.github.com>
2023-06-14 08:41:08 +0200

    Update presentation_text.js
    Nouveau texte d'introduction

--- public/texts/presentation_text.js
+++ public/texts/presentation_text.js
@@ -5,11 +5,12 @@ var presentation_texts = {
         de la biodiversité dans une perspective écologique, linguistique et culturelle. Cette création est fondée sur une \
         approche de recherche-action collaborative informée par la recherche en sociolinguistique et en didactique du plurilinguisme. \
         Elle aborde le lien entre la perte de la flore et la disparition des langues par le biais d'un récit à plusieurs voix.\
-        <br>\
-        Dans un monde dystopique, un mouvement de résistance se développe contre un régime politique qui applique une politique \
-        monolingue appauvrissant à la fois la langue et le monde naturel. Ce régime semble en effet contribuer à la disparition des \
-        mots ainsi que de la réalité que ceux-ci façonnent et reflètent. Toutefois des voix s'élèvent dans des messages publiés en ligne, \
-        dans toutes les langues. Ces messages s'interconnectent instantanément en réseau et dessinent un graphe organique et dynamique. \
-        Comment tout cela va-t-il finir ? Cela dépend de vous !</p>"
+        </p>\
+        Dans un monde dystopique, un mouvement populaire de résistance se développe contre une institution toute puissante, \
+        le CMC, en réaction à des mesures politiques qui révèlent et accélèrent l’appauvrissement culturel, tant naturel que linguistique. \
+        Cette institution contribue en effet à la disparition des langues et de leur vocabulaire, ainsi que de la réalité écologique \
+        que les mots façonnent. Toutefois des voix s'élèvent dans des messages publiés en ligne, dans des langues dont la diversité \
+        cherche à faire renaître la biodiversité. Clameurs ou murmures, ces voix s'interconnectent instantanément en réseau dans un graphe \
+        organique et dynamique. Le soulèvement populaire va-t-il renverser le pouvoir en place ? Cela dépend avant tout de vous !</p>"
 
 }
